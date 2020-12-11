8ae3e9ead7a18e7a01f3ce9417ace74870d0e600
Nikolai <axrevolution@yandex.ru>
2020-12-12 01:51:50 +0300

    Initial commit

---
+++ SKOVIKOV/simbirsoft/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic; //для IReadOnlyList
+using OpenQA.Selenium; //сам селениум
+using OpenQA.Selenium.Chrome; //для хрома
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI; //для ожидания
+
+namespace simbirsoft
+{
+    class Program : LoginPage
+    {
+        static string Login = "nikskov73@gmail.com"; //свой логин
+        static string Password = "simbirsoft12345"; //свой пароль
+        static string From = "Команда Google"; //от кого пришли письма
+        static string To = "skovikov73@yandex.ru"; //кому отправлять
+        static void Main()
+        {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            IWebDriver browser = new RemoteWebDriver(new Uri("http://127.0.0.1:8090/wd/hub"), chromeOptions);
+            browser.Manage().Window.Maximize(); //увелечение окна браузера
+            GoLoginPage(browser);
+            EnterLogin(Login);
+            GoPasswordPage(browser);
+            EnterPassord(Password);
+            GoMailPage(browser);
+            FindMails(From);
+            SendMail(To);
+        }
+    }
+    class LoginPage : PasswordPage
+    {
+        static IWebDriver browser;
+        static WebDriverWait wait;
+        static IWebElement element;
+        protected static void GoLoginPage(IWebDriver driver)
+        {
+            browser = driver;
+            wait = new WebDriverWait(browser, TimeSpan.FromSeconds(5)); //ожидание элемента в 5 сек
+            browser.Navigate().GoToUrl("https://mail.google.com/"); //переход на страницу
+        }
+        protected static void EnterLogin(string login)
+        {
+            element = wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("identifierId"))); //поиск почты
+            element.SendKeys(login + OpenQA.Selenium.Keys.Enter); //ввод почты
+        }
+    }
+    class PasswordPage : MailPage
+    {
+        static IWebDriver browser;
+        static WebDriverWait wait;
+        static IWebElement element;
+        protected static void GoPasswordPage(IWebDriver driver)
+        {
+            browser = driver;
+            wait = new WebDriverWait(browser, TimeSpan.FromSeconds(5));
+        }
+        protected static void EnterPassord(string password)
+        {
+            element = wait.Until(ExpectedConditions.ElementToBeClickable(By.Name("password"))); //поиск пароля
+            element.SendKeys(password + OpenQA.Selenium.Keys.Enter); //ввод пароля
+        }
+    }
+    class MailPage
+    {
+        static IWebDriver browser;
+        static WebDriverWait wait;
+        static IWebElement element;
+        static IReadOnlyList<IWebElement> mails;
+        protected static void GoMailPage(IWebDriver driver)
+        {
+            browser = driver;
+            wait = new WebDriverWait(browser, TimeSpan.FromSeconds(10));
+        }
+        protected static void FindMails(string From)
+        {
+            mails = wait.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.XPath("//div[@class='yW']//span[@name='"+From+"']"))); //формирование списка с нужными письмами
+        }
+        protected static void SendMail(string To)
+        {
+            element = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//div[@class='aic']"))); //поиск кнопки написать
+            element.Click(); // нажимаем на написать
+            element = wait.Until(ExpectedConditions.ElementIsVisible(By.Name("to"))); //поиск получателя
+            element.SendKeys(To); //ввод почты
+            element = wait.Until(ExpectedConditions.ElementIsVisible(By.Name("subjectbox"))); //поиск темы
+            element.SendKeys("Тестовое задание. Сковиков"); //ввод темы
+            element = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//div[@class='Ar Au']/div"))); //поиск основного текста
+            element.SendKeys(mails.Count.ToString()); //ввод текста
+            element = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//div[@class='J-J5-Ji btA']"))); //поиск кнопки отправить
+            element.Click(); //отправляем
+        }
+    }
+}
+
---
+++ SKOVIKOV/test/simbirsoft_test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("simbirsoft_test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("simbirsoft_test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c8fc888a-10e5-4176-8bbe-f303c71f0129")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
